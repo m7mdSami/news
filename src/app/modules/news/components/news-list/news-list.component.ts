5c12b20dd447b114b0ac765c23a0f284070f7589
Mohamed Sami <mohamed.sami@intercom.com.eg>
2021-10-08 03:31:59 +0200

    Edit services

--- src/app/modules/news/components/news-list/news-list.component.ts
+++ src/app/modules/news/components/news-list/news-list.component.ts
@@ -299,8 +299,8 @@ export class NewsListComponent implements OnInit, OnDestroy {
 
    getNews() {
       forkJoin([
-         this.articles$.pipe(map((e: any) => e.articles)),
-         this.categories$.pipe(
+         this.service.get<any[]>(APIs().newsListing).pipe(map((e: any) => e.articles)),
+         this.service.get<any[]>(APIs().newsCategoryListing).pipe(
             map((e: any) =>
                e.sourceCategory.reduce((acc: any, cur: any) => {
                   return { ...acc, [cur.id]: cur.name }
